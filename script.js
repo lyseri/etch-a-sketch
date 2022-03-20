0bec17efe237b82bf88115a2fea6389554f1cfd4
lyseri <cbmer@tutanota.com>
2022-03-20 13:39:50 -0600

    Make js generate a 16x16 grid of divs

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const body = document.querySelector('#grid-container');
+const contain = document.querySelector('#container');
 
 
+// Generates 16x16 grid within #container
+for (let i = 0; i < 16; i++) {
+    const gridRow = document.createElement('div');
+    contain.appendChild(gridRow);
+    gridRow.classList.add('row');
+    for (let i = 0; i < 16; i++) {
+        const gridSquare = document.createElement('div');
+        gridRow.appendChild(gridSquare);
+    }
+}
