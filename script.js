f198ad3e7a3d0724ab2a4ad82e700329dcdc4172
lyseri <cbmer@tutanota.com>
2022-03-20 14:40:44 -0600

    Add dynamic resizing of grid for window size

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const contain = document.querySelector('#container');
+const container = document.querySelector('#container-a');
 
+// Checks for larger height or width and changes container id accordingly
+window.addEventListener('resize', () => {
+    if (window.innerHeight < window.innerWidth) {
+        container.id = 'container-b';
+    } else if (window.innerHeight > window.innerWidth) {
+        container.id = 'container-a';
+    }
+});
 
 // Generates 16x16 grid within #container
 for (let i = 0; i < 16; i++) {
     const gridRow = document.createElement('div');
-    contain.appendChild(gridRow);
+    container.appendChild(gridRow);
     gridRow.classList.add('row');
     for (let i = 0; i < 16; i++) {
         const gridSquare = document.createElement('div');
